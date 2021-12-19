c5e0f8685d87841e64ec728b66c9960ea64b5e8f
raolinhu <1145333729@qq.com>
2021-12-19 21:09:47 +0800

    syscall num

--- rename.c
+++ rename.c
@@ -5,12 +5,14 @@ int main(){
 	int ret;
 	char oldname[] = "a.txt";
 	char newname[] = "b.txt";
-	ret = syscall(SYS_rename, oldname, newname);
+	// ret = syscall(SYS_rename, oldname, newname);
+	ret = syscall(82, oldname, newname);
 	if(ret == 0)
 		printf("rename successfully\n");
 	else
 		printf("rename failed\n");
 	// write(1, "hello", 7);
-	ret = syscall(SYS_write, 1, "world\n", 8);
+	// ret = syscall(SYS_write, 1, "world\n", 8);
+	ret = syscall(1, 1, "world\n", 8);
 	return 0;
 }
